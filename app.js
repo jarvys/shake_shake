6f906167f1d6e4d2030bc9dc59465e7f274d86a1
Jarvys <yuhan534@126.com>
2014-12-13 19:37:19 +0800

    save

--- app.js
+++ app.js
@@ -6,68 +6,93 @@ var async = require('async');
 var app = express();
 var apirouter = express.Router();
 
-mongoose.connect('mongodb://localhost/'+dbname);
+mongoose.connect('mongodb://localhost/' + dbname);
 
 var models = require('./model'),
-    Gift = models.Gift,
-    User = models.User,
-    Winner = models.Winner;
+  Gift = models.Gift,
+  User = models.User,
+  Winner = models.Winner;
 
-function GiftError(msg){
-    var error = new Error(msg);
-    error.msg = msg;
-    return error;
+function GiftError(msg) {
+  var error = new Error(msg);
+  error.msg = msg;
+  return error;
 }
 
-var shakeQueue = async.queue(function(callback){
-    async.waterfall([ 
-          function(callback){
-                Gift.find({},function(e,gifts){
-                    if(e) return callback(e);
-                    
-                    var result = 0;
-                    var pos = Math.random();
+var shakeQueue = async.queue(function(callback) {
+  async.waterfall([
+    function(callback) {
+      Gift.find({}, function(e, gifts) {
+        if (e) return callback(e);
 
-                    console.log(pos);
-               
-                    for(var i = 0; i < gifts.length; i++){
-                        result+= gifts[i].percent;
-                        if(pos<result && result > 0){
-                            callback(null, gifts[i]);
-                            console.log("you got a gift:");
-                            i=gifts.length;
-                        }
-                    }
-                });
-             }
-          ],function(e,gift){
-                if(e){
-                  callback(e); 
-                }else {
-                   Winner.create({
-                        gift: gift.id,
-                        date: new Date()
-                   },function(e){
-                            gift.amount = Math.max(gift.amount - 1,0);
-                            if(gift.amount === 0 ) gift.percent = 0;
+        var result = 0;
+        var pos = Math.random();
 
-                            gift.save(function(e){
-                               console.log(gift);
-                            });
-                        });    
-                }
-         });
+        console.log(pos);
+
+        for (var i = 0; i < gifts.length; i++) {
+          var gift = gifts[i];
+          if (gift.amount === 0) {
+            continue;
+          }
+
+          result += gifts[i].percent;
+          if (result > pos) {
+            console.log("you got a gift:");
+            return callback(null, gifts[i]);
+          }
+        }
+
+        callback(null, null);
+      });
+    }
+  ], function(e, gift) {
+    if (e) {
+      return callback(e);
+    }
+
+    Winner.create({
+      gift: gift.id,
+      date: new Date()
+    }, function(e, winner) {
+      if (e) {
+        return callback(e);
+      }
+
+      gift.amount = Math.max(gift.amount - 1, 0);
+      if (gift.amount === 0) gift.percent = 0;
+
+      gift.save(function(e) {
+        if (e) {
+          return callback(e);
+        }
+
+        callback(null, {
+          gift: gift,
+          winner: winner
+        });
+      });
+    });
+  });
 }, 1);
 
-app.get('/',function(req,res){
-   shakeQueue.push({},
-   function(e){
-       console.log(e);
-   });
+app.post('/api/lottery', function(req, res) {
+  shakeQueue.push({}, function(e, data) {
+    if (e) {
+      return res.json({
+        code: 1001
+      });
+    } else {
+      return res.json({
+        code: 0,
+        gift: data.gift ? data.gift.gtype : null,
+        winner: data.winner._id
+      });
+    }
+  });
 });
 
 var port = 9876;
-app.listen(port , function(){
-    console.log("listening on:"+port);
+app.listen(port, function() {
+  console.log("listening on:" + port);
 });
-
